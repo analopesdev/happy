7f6703a3e62d69f58e19602861e44fd525061f9f
analopesdev <analopesdemorais@outlook.com>
2020-10-12 13:00:05 -0300

    create routes and Single Page Application

---
+++ web/src/routes.tsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import React from 'react';
+
+// Pages
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+function Routes(){
+  return (
+    // Por dentro de todas as rotas é necessario ter o BrowserRouter
+    <BrowserRouter>
+
+      {/* O Switch vai fazer com que apenas uma unica rota seja chamada */}
+      <Switch>
+        {/* exact é usado vai fazer uma comparação de igualdade ou seja, se "/" == "/" */}
+        <Route path="/" exact component={Landing}/>
+        <Route path="/app" component={OrphanagesMap}/>
+      </Switch>
+
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
