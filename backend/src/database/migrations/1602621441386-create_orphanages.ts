3d3fa8bdef8bc79a71b52ea172c2c84936346e75
analopesdev <analopesdemorais@outlook.com>
2020-10-13 17:55:05 -0300

    create table and coluns of the orphanages table

--- backend/src/database/migrations/1602621441386-create_orphanages.ts
+++ backend/src/database/migrations/1602621441386-create_orphanages.ts
@@ -1,11 +1,63 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class createOrphanages1602621441386 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Realizar alterações
+    // Criar uma tabela, Criar um novo campo, Deletar algum campo
+    await queryRunner.createTable(new Table({ 
+        name: 'orphanages',
+        columns:[
+          {
+            name: 'id',
+            // Inteiro
+            type: 'integer',
+            // Aceitar somente numeros positivos
+            unsigned: true,
+            // Chave primaria
+            isPrimary: true,
+            // Coluna gerada automaticamente
+            isGenerated: true,
+            // Coluna gerada automaticamente usando uma lógica incremental
+            generationStrategy: 'increment'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision:2
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision:2
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            // Padrão false
+            default: false
+          }
+        ]
+      }
+    ))
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Desfazer o que foi feito no método up
+    await queryRunner.dropTable('orphanages')
+  }
 
 }
