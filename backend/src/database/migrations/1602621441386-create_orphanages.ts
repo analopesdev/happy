df01ba8f2c084619e9fe962e2cdf087d0f92d862
analopesdev <analopesdemorais@outlook.com>
2020-10-13 17:38:04 -0300

    created migrate orphanages

---
+++ backend/src/database/migrations/1602621441386-create_orphanages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createOrphanages1602621441386 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
