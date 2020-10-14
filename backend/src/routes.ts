b43e7045dd6130abb9313bad15b6c2753540afdc
analopesdev <analopesdemorais@outlook.com>
2020-10-13 23:20:55 -0300

    config upload images

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,15 @@
 import {Router} from 'express';
+import multer from 'multer'
+
 import OrphanagesController from './controllers/OrphanagesController';
+import uploadConfig from './config/upload'
 
 const routes = Router();
+const upload = multer(uploadConfig)
 
 // index, show, create, update, delete
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
+routes.post('/orphanages', upload.array('images'),OrphanagesController.create);
 
 export default routes;
