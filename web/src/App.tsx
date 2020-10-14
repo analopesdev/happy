e0d24ceb014d9628c197877a6cad0588290ea7aa
analopesdev <analopesdemorais@outlook.com>
2020-10-14 15:06:50 -0300

    fetching backend API with axes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-
-import './styles/global.css';
 import Routes from './routes';
+
+import 'leaflet/dist/leaflet.css';
+import './styles/global.css';
 
 function App() { 
   return (
