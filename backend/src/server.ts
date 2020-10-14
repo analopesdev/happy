6adeab3de544aab8b81dae2e65b14b074d32b7bc
analopesdev <analopesdemorais@outlook.com>
2020-10-14 09:40:42 -0300

    validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import path from 'path';
 import 'express-async-errors'
+import cors from 'cors'
 // Fazer com que o servidor responda a conexão
 import './database/connection'
 import routes from './routes';
 import errorHandler from './errors/handler';
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
